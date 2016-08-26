29fe43a2e03e7db9be97536dd46cdd565be14525
cyril mac philip <cyrilmac92@yahoo.com>
2016-08-27 00:31:34 +0530

    initial commit

---
+++ src/main/java/com/classfound/feedback/ClassfoundFeedbackServiceApplication.java
@@ -0,0 +1,12 @@
+package com.classfound.feedback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassfoundFeedbackServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassfoundFeedbackServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/classfound/feedback/ClassfoundFeedbackServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.classfound.feedback;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ClassfoundFeedbackServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
